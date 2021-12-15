1e9a19fee21bc2411a8f041818fabd64715b6896
levit123 <LeviCaseBlodgett@gmail.com>
2021-12-14 17:29:22 -0800

    Mines + Calculate Nearby Mines
    -added function to get the chosen spot in the minefield
    -added function to randomly place 10 mines in the minefield
    -added function to calculate the number of mines near the current spot
    -added function to repeatedly call the above function, to calculate the number of nearby mines for all spots
    -added graphic for Mines in current spot
    -added graphics for the numbers of mines that are near each spot
    
    Next step is to determine if the player wins or loses

--- JS/script.js
+++ JS/script.js
@@ -42,7 +42,166 @@ function createMinefield() {
 		//pushes the current row to the collection of rows in the minefield
 		minefield.rows.push(row);
 	}
-
+	//calls the function to randomly place 10 mines in the minefield
+	placeRandomMines(minefield);
+	//calls the function to calculate the number of nearby mines for all spots in the minefield
+	calculateAllNumbers(minefield);
 	//returns the entire minefield once it's finished being created
 	return minefield;
+}
+
+
+//function to gather information about a specific square/spot
+function getSpot(minefield, row, column)
+{
+	//returns the specific row and spot number of the chosen spot in the minefield
+	return minefield.rows[row].spots[column];
+}
+
+//adds mines to random spots in the minefield
+function placeRandomMines(minefield)
+{
+	//uses a "for" loop to choose 10 random spots to place mines in
+	for (let i = 0; i < 10; i++)
+	{
+		//chooses a random row number
+		let row = Math.round(Math.random() * 8);
+		//chooses a random spot number
+		let column = Math.round(Math.random() * 8);
+		//grabs the specific spot at that row and spot number
+		let spot = getSpot(minefield, row, column);
+		//puts a mine in that spot
+		spot.content = "mine";
+	}
+}
+
+//function for calculating the number of mines near the chosen square/spot
+function calculateNumber(minefield, row, column) {
+	//gets the current spot in the minefield
+	let thisSpot = getSpot(minefield, row, column);
+
+	//if this spot contains a mine, we can't place a number here
+	if (thisSpot.content == "mine")
+	{
+		return;
+	}
+
+	//starts the variable for holding the number of nearby mines at 0
+	let mineCount = 0;
+
+	//checks if this is not the first row of the minefield
+	if (row > 0)
+	{
+		//checks if this is not the first column of the minefield
+		if (column > 0)
+		{
+			//gets the spot 1 above and to the left of the current spot
+			let spot = getSpot(minefield, row - 1, column - 1);
+			//if that nearby spot has a mine in it, increment "mineCount" by 1
+			if (spot.content == "mine")
+			{
+				mineCount++;
+			}
+		}
+
+		//gets the spot 1 above the current spot
+		let spot = getSpot(minefield, row - 1, column);
+		//if that nearby spot has a mine in it, increment "mineCount" by 1
+		if (spot.content == "mine")
+		{
+			mineCount++;
+		}
+
+		//checks if this is not the last column of the minefield
+		if (column < 8)
+		{
+			//gets the spot 1 above and to the right of the current spot
+			let spot = getSpot(minefield, row - 1, column + 1);
+			//if that nearby spot has a mine in it, increment "mineCount" by 1
+			if (spot.content == "mine")
+			{
+				mineCount++;
+			}
+		}
+	}
+
+	//checks if this is not the first column of the minefied
+	if (column > 0)
+	{
+		//gets the spot 1 to the left of the current spot
+		let spot = getSpot(minefield, row, column - 1);
+		//if that nearby spot has a mine in it, increment "mineCount" by 1
+		if (spot.content == "mine")
+		{
+			mineCount++;
+		}
+	}
+
+	//checks if this is not the last column of the minefield
+	if (column < 8)
+	{
+		//gets the spot 1 to the right of the current spot
+		let spot = getSpot(minefield, row, column + 1);
+		//if that nearby spot has a mine in it, increment "mineCount" by 1
+		if (spot.content == "mine")
+		{
+			mineCount++;
+		}
+	}
+
+	//checks if this is not the last row of the minefield
+	if (row < 8)
+	{
+		//checks if this is not the first column of the minefield
+		if (column > 0)
+		{
+			//gets the spot 1 below and to the left of the current spot
+			let spot = getSpot(minefield, row + 1, column - 1);
+			//if that nearby spot has a mine in it, increment "mineCount" by 1
+			if (spot.content == "mine")
+			{
+				mineCount++;
+			}
+		}
+
+		//gets the spot 1 below the current spot
+		let spot = getSpot(minefield, row + 1, column);
+		//if that nearby spot has a mine in it, increment "mineCount" by 1
+		if (spot.content == "mine")
+		{
+			mineCount++;
+		}
+
+		if (column < 8)
+		{
+			//gets the spot 1 below and to the right of the current spot
+			let spot = getSpot(minefield, row + 1, column + 1);
+			//if that nearby spot has a mine in it, increment "mineCount" by 1
+			if (spot.content == "mine")
+			{
+				mineCount++;
+			}
+		}
+	}
+
+	//if the number of nearby mines is greater than 0, display that number of nearby mines to the chosen spot
+	if (mineCount > 0)
+	{
+		thisSpot.content = mineCount;
+	}
+}
+
+//function to calculate the number of nearby mines for all spots in the minefield
+function calculateAllNumbers(minefield)
+{
+	//iterates through the columns of the minefield
+	for (let y = 0; y < 9; y++)
+	{
+		//iterates through the minefield
+		for (let x = 0; x < 9; x++)
+		{
+			//calculate the number of mines that are near the current spot in this row and column
+			calculateNumber(minefield, x, y);
+		}
+	}
 }
